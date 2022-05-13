2c7d3d5b8e3c68fe34ca093be8d007295549506b
Yubaraj Magar <yubaraj.100mgr@gmail.com>
2022-05-13 19:49:47 +1000

    this project has all the functions but only works while cliking button single by single

--- script.js
+++ script.js
@@ -10,14 +10,27 @@ const wrapper = document.querySelector(".wrapper");
 const details = document.querySelector(".details");
 
 const user = [
-  { name: "Sun Maya", netWorth: 9000000 },
-  { name: "Birkha", netWorth: 7000000 },
+  { name: "Sun Maya", netWorth: 900000 },
+  { name: "Birkha", netWorth: 700000 },
   { name: "Amon", netWorth: 6000000 },
-  { name: "Yubaraj", netWorth: 3000000 },
-  { name: "Sabina", netWorth: 700000 },
+  { name: "Yubaraj", netWorth: 50000 },
+  { name: "Sabina", netWorth: 2000000 },
   { name: "Sneha", netWorth: 900000 },
   { name: "Divya", netWorth: 3000000 },
   { name: "Bibash", netWorth: 300000 },
+  { name: "Bikash", netWorth: 3000243 },
+  { name: "Anmol", netWorth: 30003240 },
+  { name: "Ram", netWorth: 30000340 },
+  { name: "Prem", netWorth: 3003240 },
+  { name: "Syam", netWorth: 3000234 },
+  { name: "Bishal", netWorth: 305400 },
+  { name: "Sanjay", netWorth: 3434000 },
+  { name: "Mahmud", netWorth: 980000 },
+  { name: "Priyanka", netWorth: 370000 },
+  { name: "Ashish", netWorth: 36000 },
+  { name: "Pradeep", netWorth: 20000 },
+  { name: "Keshab", netWorth: 30000 },
+  { name: "Himal", netWorth: 70000 },
 ];
 
 let newAddedUser = [];
@@ -44,9 +57,37 @@ const doubleUserMoney = () => {
     listofLi[i].innerHTML = `$ ${double[i]}`;
   }
 };
-
 doubleMoney.addEventListener("click", doubleUserMoney);
 // Finish Double User Money
+
+// Start Show Only Millionaires
+const onlyMillionaries = () => {
+  const onlyMilli = newAddedUser.filter((e) => e > 1000000);
+  const listofLi = document.querySelectorAll("#wealth li");
+  const listofPersonLi = document.querySelectorAll("#person li");
+  for (let i = 0; i < listofLi.length; i++) {
+    listofLi[i].innerHTML = `$ ${onlyMilli[i]}`;
+    if (onlyMilli[i] == undefined) {
+      listofLi[i].innerHTML = ``;
+      listofPersonLi[i].innerHTML = "";
+    }
+  }
+};
+showOnlyMillionaires.addEventListener("click", onlyMillionaries);
+//End Millionaires
+
+//Sort by Richest Start
+const sortRichFirst = () => {
+  const sort = newAddedUser.sort((a, b) => b - a);
+  const listofLi = document.querySelectorAll("#wealth li");
+  const listofPersonLi = document.querySelectorAll("#person li");
+  for (let i = 0; i < listofLi.length; i++) {
+    listofLi[i].innerHTML = `$ ${sort[i]}`;
+  }
+};
+
+sortByRichest.addEventListener("click", sortRichFirst);
+//Sort by Richest End
 
 //Start Add All Wealth
 const totalSum = () => {
